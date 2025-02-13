ded02f5530de14182e4da5e14432703b53138a49
eMode18 <ewagandi@gmail.com>
2025-02-13 19:36:40 +0300

    added Navbar and fixed clip-path radius in Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -76,7 +76,7 @@ const Hero = () => {
   useGSAP(() => {
     gsap.set("#video-frame", {
       clipPath: "polygon(14% 0%, 72% 0%, 90% 90%, 0% 100%)",
-      borderRadius: "0 0  40% 10%",
+      borderRadius: "0 0  15% 5%",
     });
 
     gsap.from("#video-frame", {
